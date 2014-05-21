9363ee76e77c7d4640cc16aab888f1632105c3e8
Esteban Campostrini <lesteban22@gmail.com>
2014-05-21 20:38:10 -0300

    Implementing the algorithm

--- causalization_algorithm.cpp
+++ causalization_algorithm.cpp
@@ -34,8 +34,70 @@ CausalizationStrategy::CausalizationStrategy(CausalizationGraph g){
 
 void
 CausalizationStrategy::causalize(){	
-
-
-	
-			
+	list<Vertex>::iterator iter;
+	foreach(iter, equationDescriptors){
+		Vertex eq = current_element(iter);
+		EquationType eqType = graph[current_element(iter)].eqType;
+		if(eqType == EQEQUALITY){
+			/*here we use the classic version of the algorithm*/
+			if(out_degree(eq, graph) == 1){
+				Edge e = *out_edges(eq, graph).first;			
+				Vertex unknown = target(e,graph);
+				if (e.idexes.empty()){
+					/*its a regular variable*/		
+					remove_out_edge(unknown, boost::lambda::_1 != e, graph);
+					/*TODO MAKECAUSAL*/
+					equationNumber--;
+					unknownNumber--;
+					equationDescriptors->erase(iter);
+					unknownDescriptors->remove(unknown);
+				}else{
+					/*its an array*/		
+					assert(e.indexes.size() == 1);
+					/*TODO MAKECAUSAL*/
+					/*TODO remove_index_from_array*/
+					remove_edge_from_array(unknown, e);
+					equationNumber--;
+					if(--unknownNumber == 0){
+						unknownDescriptors->remove(unknown);
+					}
+					equationDescriptors->erase(iter);
+				}
+			}else if(out_degree(eq, graph) == 0){
+				ERROR("Problem is singular, not supported yet\n");		
+			}
+		}else if(eqType == EQFOR){
+			if(out_degree(eq,graph) == 1){
+				Edge e = *out_edges(eq, graph).first;
+				Vertex unknown = target(e, graph);
+				if(e.indexes.empty()){
+					//its a regular variable and we just solve 
+					//arrays in the FOR
+				}else{
+					/*only one variable in the FOR, we causalize it*/
+					/*TODO MAKECAUSAL*/
+					remove_edge_from_array(unknown, e);
+					equationNumber -= eq.count;
+					unknownNumber -= eq.count; //the array may have more variables 
+					if(unknownNumber == 0){
+						unknownDescriptors->remove(unknown);
+					}
+					equationDescriptors->erase(iter);
+				}
+			}else{
+				/*if only one of the edges has weight == size of for range
+				* then thats the one we are causalizing */		
+				CausalizationGraph::edge_iterator begin, end;
+				Edge e = null_vertex(); 
+				for(tie(begin, end) = out_edges(eq, graph); begin != end; begin++){
+					Edge e = *begin;		
+					Vertex unknown = target(e, graph);
+					if(e.indexes.size() == )
+				}
+			}
+		}else{
+			ERROR("CausalizationStrategy::causalize:"
+			      "Equation type not supported\n");		
+		}
+	}
 }
